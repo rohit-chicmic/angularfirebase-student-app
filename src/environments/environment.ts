317debe0bfd7de0ece11124d36fec6502eb9c971
rohit-chicmic <rohit.kumar@chicmic.co.in>
2021-06-10 10:00:44 +0530

    added firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAkH8edRyQOx74XW93FEiUwj7tA0p0CmwQ",
+    authDomain: "angularfirebase-student-1c427.firebaseapp.com",
+    databaseURL: "https://angularfirebase-student-1c427-default-rtdb.firebaseio.com",
+    projectId: "angularfirebase-student-1c427",
+    storageBucket: "angularfirebase-student-1c427.appspot.com",
+    messagingSenderId: "80216372317",
+    appId: "1:80216372317:web:05845c0f29c90b13f709b4",
+    // measurementId: "G-9K4787ZFD9"
+  }
 };
 
 /*
